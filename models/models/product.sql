b1afb0eca2b2f9b221c43774c262937b98c37c5f
Wallace Graça <wallacecpdg@hotmail.com>
2023-10-29 19:34:40 +0000

    ajuste na query

--- models/models/product.sql
+++ models/models/product.sql
@@ -1,25 +1,6 @@
 with source_data as (
     select
-        productid
-        , name as product_name
-        , safetystocklevel
-        , finishedgoodsflag
-        , class
-        , makeflag
-        , productnumber
-        , reorderpoint
-        , modifieddate
-        , rowguid
-        , productmodelid
-        , weightunitmeasurecode
-        , standardcost
-        , productsubcategoryid
-        , listprice
-        , daystomanufacture
-        , productline
-        , color
-        , sellstartdate
-        , weight as product_weight
+        *
     from {{ source('adventureworks-gcp', 'product') }}
 )
 select *
